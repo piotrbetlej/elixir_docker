04144eae58ce1b15f396f8619c16681e76e668ee
Piotr Betlej <piotr.betlej@googlemail.com>
2019-10-06 22:05:28 +0200

    Added container prune for dangling container names.

--- docker_start.sh
+++ docker_start.sh
@@ -6,6 +6,7 @@ SHARED_DIR="elixir_in_container"
 PWD=$(pwd)
 
 # sudo docker run -v ${PWD}/${SHARED_DIR}:/usr/local/elixir_ -it debian_buster:elixir /bin/bash
+sudo docker container prune
 sudo docker run --name elixir_container -it debian_buster:elixir /bin/bash
 
 # set +x
